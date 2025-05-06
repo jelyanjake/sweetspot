a3a8eef71c52c31f38c5dc5bb10851aa2e5dd951
osama been lagging <112924068+jelyanjake@users.noreply.github.com>
2025-05-06 10:20:22 +0800

    a

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import StatusModal from './StatusModal';
 import { sendSMS } from './sms';
-import bg from './assets/bg.jpg';
+import bg from '../assets/bg.jpg';
 
 function HomePage() {
   const [status, setStatus] = useState(null);
