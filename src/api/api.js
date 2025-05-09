c3b49fbeaeb564ab45e0822ef3448832da3fbd10
jelyanjake <jelyanjake@gmail.com>
2025-05-10 01:59:14 +0800

    upd api

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 //backup https://67f50ba7913986b16fa2f9ff.mockapi.io/api/v1/burgers
+//backup2 https://68103cb727f2fdac2410a610.mockapi.io/api/elective2/burgers
 
 export const fetchBurgers = async () => {
-  const response = await axios.get('https://68103cb727f2fdac2410a610.mockapi.io/api/elective2/burgers');
+  const response = await axios.get('https://67f50ba7913986b16fa2f9ff.mockapi.io/api/v1/burgers');
   return response.data;
 };
