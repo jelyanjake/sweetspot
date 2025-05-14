373fe7ff0b834a4118bdd1df8be9f087a4a65dde
jelyanjake <jelyanjake@gmail.com>
2025-05-14 23:28:26 +0800

    dwdw

--- src/components/PasswordModal.jsx
+++ src/components/PasswordModal.jsx
@@ -2,26 +2,27 @@ import { useState } from 'react';
 import './PasswordModal.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { API } from './h';
+import API from './h';
 
 export const PasswordModal = ({ onSuccess }) => {
   const [password, setPassword] = useState('');
   const [user, setUser] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
+  const [user1, setUser1] = useState('');
+  const [pass1, setPass1] = useState('');
 
   const fetchData = async () => {
       try {
         const response = await axios.get(API);
-        users = response.data;
+        const users = response.data;
+        setUser1(users.user);
+        setPass1(users.password);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
     fetchData();
-
-  const user1 = users.name;
-  const pass1 = users.password;
 
   const onClose = () => {
     navigate('/');
--- src/components/h.js
+++ src/components/h.js
@@ -1,1 +1,3 @@
 const API = 'https://67f50ba7913986b16fa2f9ff.mockapi.io/api/v1/users/7';
+
+export default API;
